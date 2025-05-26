9facc1668bcb385737fdceb6fb5b3f913bd2beab
andrew-Linsanity <andrew.preferredandy@gmail.com>
2025-05-26 18:33:29 +0800

    create item model

---
+++ item/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    image_urls = models.JSONField(default=list, blank=True)
---
+++ item/urls.py
@@ -0,0 +1,11 @@
+# items/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),  # This gives you /items/, /items/<id>/ etc.
+]
