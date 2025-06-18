3d57b06e695dcde923b3e61056571340eda9c15f
TAEEUN KIM <taeeunk1208@gmail.com>
2025-06-18 17:58:43 +0900

    user registration setup

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .serializers import CustomTokenObtainPairSerializer
+from .serializers import CustomTokenObtainPairSerializer, RegisterSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -12,3 +14,9 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     """
     serializer_class = CustomTokenObtainPairSerializer
     permission_classes = (AllowAny,)
+
+
+class RegisterView(generics.CreateAPIView): # for create-only endpoints
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
