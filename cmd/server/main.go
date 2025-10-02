c9404d209327e0ecf368ab083d1fb9965f40b0b2
Ethan Lee <ethantlee21@gmail.com>
2025-10-02 15:08:28 -0400

    setup sqlc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "log/slog"
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/dooleyonline/backend/db"
+	"github.com/jackc/pgx/v5"
+)
 
 func main() {
 	slog.Info("server")
+
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, "user=pqgotest dbname=pqgotest sslmode=verify-full")
+	if err != nil {
+		slog.Error("failed to connect to db", slog.Any("error", err))
+	}
+	defer conn.Close(ctx)
+
+	queries := db.New(conn)
+
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		slog.Error("failed to execute query", slog.Any("error", err))
+	}
+
+	fmt.Println(authors)
 }
