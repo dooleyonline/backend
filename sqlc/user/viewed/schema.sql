8410d3fead8a0e403301094a514f961c3a4c0950
Linsanity <andrew.preferredandy@gmail.com>
2025-11-09 01:00:56 -0500

    Removed timezone from user.viewed table's created at default value

--- sqlc/user/viewed/schema.sql
+++ sqlc/user/viewed/schema.sql
@@ -4,7 +4,7 @@ set search_path TO "user";
 create table "user"."viewed" (
   user_id uuid not null default gen_random_uuid (),
   item_id bigint not null,
-  created_at timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text)
+  created_at timestamp with time zone not null default now()
   -- constraint viewed_pkey primary key (user_id, item_id),
   -- constraint view_user_id_fkey foreign KEY (user_id) references "user"."user" (id) on update CASCADE on delete CASCADE,
   -- constraint viewed_item_id_fkey foreign KEY (item_id) references item.item (id) on update CASCADE on delete CASCADE
